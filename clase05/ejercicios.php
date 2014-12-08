bdd5b288f5c4f8391f627fd311b9de4663c60ca2
MiguelJimenez <maj009@gmail.com>
2014-12-08 13:43:48 +0100

    20141208 Libreria

---
+++ clase05/ejercicios.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejercicios</title>
+</head>
+<body>
+	<?php 
+	function imprime($cadena){
+		echo $cadena;
+	}
+
+	imprime('Hola Mundo');
+
+	 ?>
+	
+</body>
+</html>
